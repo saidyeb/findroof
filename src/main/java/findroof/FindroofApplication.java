c1bf69002f7a7f29f1f3ad1dda04ce666423bc33
said yeb <said.yebbah@hotmail.com>
2019-11-15 17:20:59 +0100

    build (project) : initialisation de l'architecture

---
+++ src/main/java/findroof/FindroofApplication.java
@@ -0,0 +1,13 @@
+package findroof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindroofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindroofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/findroof/FindroofApplicationTests.java
@@ -0,0 +1,13 @@
+package findroof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindroofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
