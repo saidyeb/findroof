0c58b7310044feb7f43a5147aaac9642698664e5
said yeb <said.yebbah@hotmail.com>
2019-11-28 18:00:04 +0100

    feat (webmvc) : adding configuration to authentification and view/controller to login and the html/css template related to. defining route navigation. adding header/footer

---
+++ src/main/java/findroof/web/config/WebMvcConfig.java
@@ -0,0 +1,39 @@
+package findroof.web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    public WebMvcConfig() {
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/webjars/**",
+                "/img/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/META-INF/resources/webjars/",
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/request").setViewName("request");
+    }
+}
---
+++ src/main/java/findroof/web/config/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package findroof.web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public WebSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean(name = "passwordEncoder2")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+ 
+    	http.authorizeRequests()
+    			.antMatchers("/request").permitAll()
+    			.antMatchers("/resources**").permitAll()
+                .anyRequest().permitAll()
+        .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/request").failureUrl("/login")
+                .usernameParameter("username").passwordParameter("password")
+        .and()
+            .logout().invalidateHttpSession(true)
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/login")
+        .and()
+            .csrf()
+        .and()
+            .sessionManagement().maximumSessions(1).expiredUrl("/login");
+    }
+}
