e7223ed637ae2f8c1e1396680f5254da4ffe3a1c
said yeb <said.yebbah@hotmail.com>
2019-12-08 01:04:06 +0100

    refact (bo, service) : add pricedollar to possession

--- src/main/java/findroof/bo/BoPossession.java
+++ src/main/java/findroof/bo/BoPossession.java
@@ -6,14 +6,22 @@ public class BoPossession {
 
 	private Possession possession;
 	
+	private double priceDollar; 
+	
 	public BoPossession() {
 		super();
+		this.priceDollar = 0.0;
 		this.possession = new Possession();
 	}
 	
 	public BoPossession(Possession possession) {
 		super();
 		this.possession = possession;
+	}
+	
+	public BoPossession(Possession possession, double priceDollar) {
+		this.possession = possession;
+		this.priceDollar = priceDollar;
 	}
 
 	public Possession getPossession() {
@@ -23,4 +31,13 @@ public class BoPossession {
 	public void setPossession(Possession possession) {
 		this.possession = possession;
 	}
+
+	public double getPriceDollar() {
+		return priceDollar;
+	}
+
+	public void setPriceDollar(double priceDollar) {
+		this.priceDollar = priceDollar;
+	}
+	
 }
